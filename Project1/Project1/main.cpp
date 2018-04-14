09ee22c10644afd47f5471511abb9d209c32eea3
PhucAnh1412 <npanh1996@gmail.com>
2018-04-14 23:47:15 +0700

    viết code để chạy ở chế độ thầy sẽ test (tạm gọi là chế độ 1)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -18,6 +18,22 @@ public:
 private:
 };
 
+class DGLop
+{
+public:
+	string name;
+	
+	int after_count;
+	int before_count;
+
+	int acc_count;
+
+	double p;
+	double r;
+	double f;
+private:
+};
+
 string tachtenclass(string s) // e.g: s = "test.txt"
 {
 	string str = "";
@@ -166,8 +182,40 @@ void phanloai(string classfolder, string testinput, string result, string mode)
 	itest.close();
 
 	k = atoi(vTest.back().c_str());
+
 	vTest.pop_back();
+
+	// lay nhan da duoc danh dau tu bo test
+	vector<string> vLabel_before;
+	for (int i = 0; i < vTest.size(); ++i)
+	{
+		string temp = "";
+		int j = vTest[i].length() - 1;
+		while (vTest[i][j] != ' ')
+		{
+			temp = vTest[i][j] + temp;
+			vTest[i].erase(vTest[i].length() - 1);
+			--j;
+		}
+		vTest[i].erase(vTest[i].length() - 1);
+		vLabel_before.push_back(temp);
+	}
+
+	vector<DGLop> vDG;
+	for (int i = 0; i < vClass.size(); ++i)
+	{
+		DGLop temp;
+		temp.name = vClass[i].name;
+		temp.before_count = 0;
+		for (int j = 0; j < vLabel_before.size(); ++j) // dem so van ban thuoc lop i truoc khi phan loai
+		{
+			if (temp.name.compare(vLabel_before[j]) == 0)
+				++temp.before_count;
+		}
+		vDG.push_back(temp);
+	}
 	// tim kiem
+	vector<string> vLabel_after;
 	int* class_count = new int[vClass.size()];
 	for (int i = 0; i < vTest.size(); ++i)
 	{
@@ -193,9 +241,53 @@ void phanloai(string classfolder, string testinput, string result, string mode)
 				}
 			}
 		}
-		ofile << vTest[i] << " " << vClass[max_pos].name << endl;
-		
+
+		vLabel_after.push_back(vClass[max_pos].name);
 	}
+	// ~Doan nay chua duoc tot lam~
+	for (int i = 0; i < vDG.size(); ++i)
+	{
+		vDG[i].acc_count = 0;
+		vDG[i].after_count = 0;
+		for (int j = 0; j < vLabel_after.size(); ++j)
+		{
+			if (vLabel_after[j].compare(vDG[i].name))
+			{
+				++vDG[i].after_count;
+				if (vLabel_after[j].compare(vLabel_before[j]) == 0)
+					++vDG[i].acc_count;
+			}
+		}
+
+		vDG[i].p = (double)vDG[i].acc_count / vDG[i].after_count;
+		vDG[i].r = (double)vDG[i].acc_count / vDG[i].before_count;
+		vDG[i].f = 2 * vDG[i].p*vDG[i].r / (vDG[i].p + vDG[i].r);
+	}
+	int C_count = vClass.size();
+	double p_macro = 0, r_macro = 0, f_macro = 0, f_micro = 0;
+	for (int i = 0; i < C_count; ++i)
+	{
+		p_macro += vDG[i].p;
+		r_macro += vDG[i].r;
+		f_micro += vDG[i].acc_count;
+	}
+	p_macro = p_macro / C_count;
+	r_macro = r_macro / C_count;
+	f_macro = 2 * p_macro*r_macro / (p_macro + r_macro);
+	f_micro = f_micro / (double)vTest.size();
+
+	for (int i = 0; i < vTest.size(); ++i)
+		ofile << vTest[i] << " " << vLabel_after[i] << endl;
+	for (int i = 0; i < vDG.size(); ++i)
+	{
+		ofile << "P" << " " << vDG[i].name << ": " << vDG[i].p * 100 << endl;
+		ofile << "R" << " " << vDG[i].name << ": " << vDG[i].r * 100 << endl;
+		ofile << "F" << " " << vDG[i].name << ": " << vDG[i].f * 100 << endl;
+	}
+
+	ofile << "F_macro: " << f_macro * 100 << endl;
+	ofile << "F_micro: " << f_micro * 100;
+
 	delete[] class_count;
 	ofile.close();
 }
